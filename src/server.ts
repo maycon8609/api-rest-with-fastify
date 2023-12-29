45f528ca9bd1e623ef1c858593ad0ab42b3e3672
maycon silva <maycon8609@gmail.com>
2023-12-29 10:46:57 -0300

    chore: configure eslint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import fastify from "fastify"
+import fastify from 'fastify'
 
 const app = fastify()
 
@@ -6,8 +6,10 @@ app.get('/hello', () => {
   return 'Hello word!!'
 })
 
-app.listen({
-  port: 3333
-}).then(() => {
-  console.log('HTTP server running in port 3333!')
-})
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP server running in port 3333!')
+  })
