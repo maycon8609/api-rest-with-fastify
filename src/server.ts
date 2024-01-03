87dae3165713ffec5c99073d27a5b838e78808ca
maycon silva <maycon8609@gmail.com>
2024-01-03 12:22:22 -0300

    feat: treating environment variables with zod

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,32 @@
+import crypto from 'node:crypto'
 import fastify from 'fastify'
+
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'Hello word!!'
+app.get('/transactions', async () => {
+  const transaction = await knex('transactions').select('*')
+
+  return transaction
+})
+
+app.post('/transactions', async () => {
+  const transaction = await knex('transactions')
+    .insert({
+      id: crypto.randomUUID(),
+      title: 'Title',
+      amount: 1000,
+    })
+    .returning('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP server running in port 3333!')
