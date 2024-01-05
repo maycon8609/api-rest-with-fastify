3b3b5d6ce45210946b16b746fff72383865a7110
maycon silva <maycon8609@gmail.com>
2024-01-05 14:08:52 -0300

    test: added tests to transactions

---
+++ test/e2e/transactions.spec.ts
@@ -0,0 +1,49 @@
+import supertest from 'supertest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '../../src/app'
+
+describe('E2E - [routes] : transactions', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to create a new transaction', async () => {
+    const response = await supertest(app.server).post('/transactions').send({
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+
+  it('should be able to list all transaction', async () => {
+    const transaction = {
+      title: 'New transaction',
+      amount: 5000,
+      type: 'credit',
+    }
+
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send(transaction)
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await supertest(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies)
+
+    expect(listTransactionsResponse.statusCode).toEqual(200)
+    expect(listTransactionsResponse.body.transactions).toEqual([
+      expect.objectContaining({
+        title: transaction.title,
+        amount: transaction.amount,
+      }),
+    ])
+  })
+})
