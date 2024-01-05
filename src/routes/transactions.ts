0ba1da3bf23ebb52b353c9f62d104b86c1e5c55d
maycon silva <maycon8609@gmail.com>
2024-01-05 14:09:18 -0300

    refactor: updated return to list transactions

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -46,11 +46,11 @@ export async function transactionsRoutes(app: FastifyInstance) {
     async (request) => {
       const { sessionId } = request.cookies
 
-      const transaction = await knex('transactions')
+      const transactions = await knex('transactions')
         .where('session_id', sessionId)
         .select('*')
 
-      return { transaction }
+      return { transactions }
     },
   )
 
